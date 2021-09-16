7fe1f1fb4f7a27791399e03d55ba4d1bc22fd207
Elliott7 <elliottbreadmore@hotmail.com>
2021-09-16 18:28:05 +1000

    Refactored to portal modal to main index.html file

--- src/components/modal/ErrorModal.js
+++ src/components/modal/ErrorModal.js
@@ -1,28 +1,46 @@
-import React, {} from 'react'
+import React from 'react'
+import ReactDOM from "react-dom";
 import "./ErrorModal.css"
 import Button from "../UI/Button";
+import Wrapper from "../Helpers/Wrapper";
 
+// Backdrop function
+function Backdrop(props) {
+    return(
+        <div className="backdrop" onClick={props.closeButton} />
+    )}
+
+// Main Modal Overlay
+function ModalOverlay(props){
+    return(
+        <div className="modal">
+            <header className="header">
+                <h2>
+                    {props.title}
+                </h2>
+            </header>
+            <div className="content">
+                <p>
+                    {props.message}
+                </p>
+            </div>
+            <footer className="actions">
+                <Button onClick={props.closeButton}>Okay</Button>
+            </footer>
+        </div>
+    )}
+
+// Main ErrorModal Componant
 function ErrorModal(props){
     return(
-        <div>
-            <div className="backdrop" onClick={props.closeButton} />
-            <div className="modal">
-                <header className="header">
-                    <h2>
-                        {props.title}
-                    </h2>
-                </header>
-                <div className="content">
-                    <p>
-                        {props.message}
-                    </p>
-                </div>
-                <footer className="actions">
-                    <Button onClick={props.closeButton}>Okay</Button>
-                </footer>
-            </div>
-        </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop closeButton={props.closeButton} />,
+                document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(
+                <ModalOverlay title={props.title} message={props.message} closeButton={props.closeButton} />,
+                document.getElementById('overlay-root'))}
+        </React.Fragment>
     )
 }
-
 export default ErrorModal
